9ba57fbae9e1cb33124df6d3cc1e35646651bd7b
Eds <steadyede@live.co.uk>
2021-06-27 14:24:41 +0100

    shows record of games from initial state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { useState } from 'react'
+
 import AddResults from './Components/Forms/AddResults'
 import './App.css';
 
 function App() {
+
+  const [results, setResults ] = useState([
+    {
+      id: 1,
+      date: '05/06/2020',
+      winner: 'Eds',
+      secondPlace: 'Sean', thirdPlace: 'Mark', fourthPlace: 'Anton', fifthPlace: 'Dan',
+      sixthPlace: 'Stef', seventhPlace: 'Jess'
+    }
+  ])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +23,15 @@ function App() {
         </p>
       </header>
       <body>
+        {results.map((result, index) => (
+          <React.Fragment>
+            <li>Date: {result.date}</li>
+            <li>Winner: {result.winner}</li>
+            <li>Second: {result.secondPlace}</li>
+            <li>Last: {result.seventhPlace}</li>
+          </React.Fragment>
+        ))}  
+
           <AddResults />
       </body>
     </div>
---
+++ src/Components/ViewResults.js
@@ -0,0 +1,20 @@
+
+// List - ordered by most recent games
+//
+// Who won
+// Ordered list of positions
+
+// if RESULTS.js.length === 0 then just ask for first entry
+
+// need to link View Results to Adding of results [VIA RESULTS.js]
+
+
+const ViewResults = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ViewResults
