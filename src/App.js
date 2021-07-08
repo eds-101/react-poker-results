783c28d8cd5a8776c780053238949810390ec178
Eds <steadyede@live.co.uk>
2021-07-08 14:14:04 +0100

    form skeleton

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AddResults from './Components/Forms/AddResults'
 import './App.css';
 
 function App() {
 
-  const [results, setResults ] = useState([
-    {
-      id: 1,
-      date: '05/06/2020',
-      winner: 'Eds',
-      secondPlace: 'Sean', thirdPlace: 'Mark', fourthPlace: 'Anton', fifthPlace: 'Dan',
-      sixthPlace: 'Stef', seventhPlace: 'Jess'
-    }
-  ])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to the Poker Results App
-        </p>
-      </header>
-      <body>
-        {results.map((result, index) => (
-          <React.Fragment>
-            <li>Date: {result.date}</li>
-            <li>Winner: {result.winner}</li>
-            <li>Second: {result.secondPlace}</li>
-            <li>Last: {result.seventhPlace}</li>
-          </React.Fragment>
-        ))}  
-          <AddResults />
-      </body>
-    </div>
+    <form 
+    // onSubmit={onSubmit}
+    >
+    <h1>Create a new Poker leaderboard</h1>
+
+    <label>Title:</label>
+    <input 
+      type="text" required
+    //   value={values.name} onChange={set('name')} 
+      />
+
+    <label>Description:</label>
+    <textarea placeholder={"Tell us about your poker league here!"} 
+    // value={values.habits} onChange={set('habits')}
+    />
+
+    <label>Number of initial players:</label>
+    <strong>You can edit, add and remove players later. Max 6 players (until live)</strong>
+    <input 
+      type="number" min="2" max="6" required
+    //   value={values.name} onChange={set('name')} 
+      />
+
+    <button type="submit">Add Player details</button>
+  </form>
+
   );
 }
 
