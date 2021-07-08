0949a431991a2c3dd65343f4e97b4b35c8ce10ae
Eds <steadyede@live.co.uk>
2021-07-08 14:43:56 +0100

    skeleton form

--- src/App.js
+++ src/App.js
@@ -2,38 +2,49 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import AddResults from './Components/Forms/AddResults'
-import './App.css';
-
 function App() {
+  const [values, setValues] = useState({
+    title: '', description: '', player_count: ''
+  })
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      alert('Your details were saved!')
+    } catch (e) {
+      alert(`Error ${e.message}`)
+    }
+  }
+
+  const set = (name) => {
+    return ({ target: { value } }) => {
+      setValues(oldValues => ({...oldValues, [name]: value}))
+    }
+  }
 
   return (
-    <form 
-    // onSubmit={onSubmit}
-    >
-    <h1>Create a new Poker leaderboard</h1>
+    <form onSubmit={onSubmit}>
+      <h1>Create a new Poker leaderboard</h1>
 
-    <label>Title:</label>
-    <input 
-      type="text" required
-    //   value={values.name} onChange={set('name')} 
+      <label>Title:</label>
+      <input 
+        type="text" required value={values.title} onChange={set('title')} 
       />
 
-    <label>Description:</label>
-    <textarea placeholder={"Tell us about your poker league here!"} 
-    // value={values.habits} onChange={set('habits')}
-    />
-
-    <label>Number of initial players:</label>
-    <strong>You can edit, add and remove players later. Max 6 players (until live)</strong>
-    <input 
-      type="number" min="2" max="6" required
-    //   value={values.name} onChange={set('name')} 
+      <label>Description:</label>
+      <textarea placeholder={"Tell us about your poker league here!"} 
+        value={values.description} onChange={set('description')}
       />
 
-    <button type="submit">Add Player details</button>
+      <label>Number of initial players:</label>
+      <strong>You can edit, add and remove players later. Max 6 players (until live)</strong>
+      <input 
+        type="number" min="2" max="6" required
+        value={values.player_count} onChange={set('player_count')} 
+      />
+
+      <button type="submit">Add Player details</button>
   </form>
-
   );
 }
 
