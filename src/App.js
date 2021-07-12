16dd4a5a0d81d32849c23f740150568dc6fb7c5f
Edeman George <71378676+eds-101@users.noreply.github.com>
2021-07-12 17:41:24 +0100

    simplified to manual, 4 player input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
-import './App.css';
-
 function App() {
   const [values, setValues] = useState({
-    title: '', description: '', playerCount: ''
+    title: '', description: '', playerCount: '',
+    player1:'', player2:'', player3:'', player4:''
   })
 
   const onSubmit = async (event) => {
@@ -45,14 +45,25 @@ function App() {
 
       <Form.Group controlId="formPlayerCount">
         <Form.Label>Number of initial players</Form.Label>
-        <Form.Control className="smallWidthInput" type="number" min="2" max="20" required size="sm"
+        <Form.Control className="smallWidthInput" type="number" min="4" max="4" required size="sm"
           value={values.player_count} onChange={set('playerCount')} />
         <Form.Text className="text-muted">
-          You can edit, add and remove players later. Maximum 20.
+          You can edit, add and remove players later. Set to 4 whilst in development.
         </Form.Text>
       </Form.Group>
 
-      <Button type="submit">Add Player details</Button>
+      <Form.Group controlId="formPlayers">
+        <Form.Control type="text" required placeholder="Name of Player 1"
+          value={values.player1} onChange={set('player1')} />
+        <Form.Control type="text" required placeholder="Name of Player 2"
+          value={values.player2} onChange={set('player2')} />
+        <Form.Control type="text" required placeholder="Name of Player 3"
+          value={values.player3} onChange={set('player3')} />
+        <Form.Control type="text" required placeholder="Name of Player 4"
+          value={values.player4} onChange={set('player4')} />
+      </Form.Group>
+
+      <Button type="submit">Create league</Button>
 
     </Form>
   );
